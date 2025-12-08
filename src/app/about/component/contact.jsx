0449df9ada353410ddb59461b6ed005d8f768c39
Muhammad Bilal Sami <101085324+bilalsami00@users.noreply.github.com>
2025-12-08 16:16:44 +0500

    clean up code attempt 2

--- src/app/about/component/contact.jsx
+++ src/app/about/component/contact.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import Image from "next/image";
 import PhoneFlagInput from "./PhoneFlagInput";
 
